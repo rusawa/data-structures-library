2544f088deda81beb22275cc45e09558cf306a09
rusawa <82612741+rusawa@users.noreply.github.com>
2025-09-27 22:20:48 +0200

    Added linked list function signatures to data-structures.h

---
+++ data-structures.h
@@ -0,0 +1,26 @@
+#ifndef __DATA_STRUCTURES_H__
+#define __DATA_STRUCTURES_H__
+
+//========================LINKED LIST========================
+typedef struct node {
+    int value;
+    struct node* next;
+} node; 
+
+
+//========================LINKED LIST========================
+node* initLinkedList();
+int printLinkedList(node* root);
+void addStart(node** root, int value);
+int addEnd(node** root, int value);
+void removeStart(node** root);
+void removeEnd(node* root);
+bool isEmpty(node* root);
+node getStart(node* root);
+node getEnd(node* root);
+void deleteByIndex(node** root, int index);
+int addAtIndex(node** root, int index, int value);
+void clearLinkedList(node** root);
+
+
+#endif
